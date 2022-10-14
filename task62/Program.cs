68260bce2818170f9e978291500ea875c7e4ce99
Svetikova Marina <m-golovleva@yandex.ru>
2022-10-14 19:51:08 +0500

    task62

---
+++ task62/Program.cs
@@ -0,0 +1,44 @@
+﻿// Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[size, size];
+
+int numbers = 1;
+int i = 0;
+int j = 0;
+
+while (numbers <= size * size)
+{
+    arr[i, j] = numbers;
+    if (i <= j + 1 && i + j < size - 1)
+        ++j;
+    else if (i < j && i + j >= size - 1)
+        ++i;
+    else if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++numbers;
+}
+
+PrintArray(arr);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
