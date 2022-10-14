471bcd602d064986cc42f85f7c00d257ce8a5a5a
Svetikova Marina <m-golovleva@yandex.ru>
2022-10-14 19:49:34 +0500

    task52

---
+++ task54/Program.cs
@@ -0,0 +1,61 @@
+﻿// // Задайте двумерный массив. Напишите программу, которая упорядочит по возрастанию элементы каждой строки двумерного массива.
+//  Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+Console.WriteLine("Введите количество строк: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[lines, columns];
+FillArray (numbers);
+Console.WriteLine();
+Console.WriteLine("Исходный массив");
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int m = 0; m < numbers.GetLength(1) - 1; m++)
+        {
+            if (numbers[i, m] > numbers[i, m + 1]) 
+            {
+                int value = 0;
+                value = numbers[i, m];
+                numbers[i, m] = numbers[i, m + 1];
+                numbers[i, m + 1] = value;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными по возрастанию элементами");
+PrintArray(numbers);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
